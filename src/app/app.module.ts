1022a56ea322b5636f464c513860306f56797a6f
crusiris <cruzsirys@gmail.com>
2020-03-24 12:08:14 -0300

    Generando componentes principales de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { PokemonesComponent } from './components/pokemones/pokemones.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PokemonesComponent,
+    PokemonComponent
   ],
   imports: [
     BrowserModule,
